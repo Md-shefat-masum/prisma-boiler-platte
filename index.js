70381b79c78685a9d8e221bf28c37fc56efcc018
Md-shefat-masum <mshefat924@gmail.com>
2023-04-23 22:42:37 +0600

    express setup

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const server = express();
+const port = 5000;
+
+server.get('/',(req,res)=>{
+    res.send('welcom to prisma-express')
+})
+
+server.listen(port,()=>{
+    console.log('server running on http://127.0.0.1:'+port);
+})
